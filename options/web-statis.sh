19a2190c0a207b69644f07a3b517e115fcfb7ac9
BangAthar <craft@gmail.com>
2023-02-24 14:05:30 +0700

    a

--- options/web-statis.sh
+++ options/web-statis.sh
@@ -5,5 +5,21 @@
 # Description : Script auto konfigurasi ini hanya digunakan untuk sistem operasi linux server yang
 #               bertujuan untuk mempermudah dan membantu pekerjaan manusia!
 ##################################################################################################
-$r
-echo -e "test"
+
+cy='\033[96m' # Cyan Color
+sc='\033[1;31m' # Red Color
+c='\e[32m' # Green Color
+tc='\033[38;5;11m' # Yellow Color
+r='tput sgr0' #Reset Color
+
+echo -e "\n\n\n\n ${cy}
+
+ _     _  _______  _______    _______  _______  _______  _______  ___   _______ 
+| | _ | ||       ||  _    |  |       ||       ||   _   ||       ||   | |       |
+| || || ||    ___|| |_|   |  |  _____||_     _||  |_|  ||_     _||   | |  _____|
+|       ||   |___ |       |  | |_____   |   |  |       |  |   |  |   | | |_____ 
+|       ||    ___||  _   |   |_____  |  |   |  |       |  |   |  |   | |_____  |
+|   _   ||   |___ | |_|   |   _____| |  |   |  |   _   |  |   |  |   |  _____| |
+|__| |__||_______||_______|  |_______|  |___|  |__| |__|  |___|  |___| |_______|
+
+"; $r
