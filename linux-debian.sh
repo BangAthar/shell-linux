a47cbd2fd433a212142ceba10fc1def9ba76ef5d
BangAthar <craft@gmail.com>
2023-02-24 09:06:30 +0700

    updates

--- linux-debian.sh
+++ linux-debian.sh
@@ -1,1 +1,57 @@
+#!/bin/bash
 
+##################################################################################################
+# Author: NoxvalDev
+# Description : Script auto konfigurasi ini hanya digunakan untuk sistem operasi linux server yang
+#               bertujuan untuk mempermudah dan membantu pekerjaan manusia!
+##################################################################################################
+
+cy='\033[96m' # Cyan Color
+rc='\033[0;31m' # Red Color
+c='\e[32m' # Green Color
+tc=$'\033[38;5;11m' # Yellow Color
+r='tput sgr0' #Reset Color
+
+echo -e "\n\n\n\n ${d}
+
+  ____  __ __  ______   ___        _____   ___ ______  __ __  ____      
+ /    ||  |  ||      | /   \      / ___/  /  _]      ||  |  ||    \     
+|  o  ||  |  ||      ||     |    (   \_  /  [_|      ||  |  ||  o  )    
+|     ||  |  ||_|  |_||  O  |     \__  ||    _]_|  |_||  |  ||   _/     
+|  _  ||  :  |  |  |  |     |     /  \ ||   [_  |  |  |  :  ||  |       
+|  |  ||     |  |  |  |     |     \    ||     | |  |  |     ||  |       
+|__|__| \__,_|  |__|   \___/       \___||_____| |__|   \__,_||__|       
+                                                                        
+"; $r
+
+echo -e "\n ${rc}
+
+ ____   __ __ 
+|    \ |  |  |
+|  o  )|  |  |
+|     ||  ~  |
+|  O  ||___, |
+|     ||     |
+|_____||____/ 
+              
+
+"; $r
+
+echo -e "\n ${rc}
+
+ ____    ___   __ __  __ __   ____  _      ___      ___ __ __ 
+|    \  /   \ |  |  ||  |  | /    || |    |   \    /  _]  |  |
+|  _  ||     ||  |  ||  |  ||  o  || |    |    \  /  [_|  |  |
+|  |  ||  O  ||_   _||  |  ||     || |___ |  D  ||    _]  |  |
+|  |  ||     ||     ||  :  ||  _  ||     ||     ||   [_|  :  |
+|  |  ||     ||  |  | \   / |  |  ||     ||     ||     |\   / 
+|__|__| \___/ |__|__|  \_/  |__|__||_____||_____||_____| \_/  
+                                                              
+" $r
+
+
+echo -n "${tc}Sebelum memulai menjalakan sistem auto konfigurasi ini pastikan anda berada dalam server linux"
+sleep 1
+echo -n "${tc}Script auto config ini hanya di khususkan untuk mengconfigurasi server linux based Debian"
+sleep 1
+echo -n "${tc}Direkomendasi untuk menggunakan script ini hanya pengguna AWS"
