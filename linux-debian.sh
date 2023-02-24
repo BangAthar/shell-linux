706220b2b891f550221271c5b251271f8cd58b53
BangAthar <craft@gmail.com>
2023-02-24 10:36:03 +0700

    a

--- linux-debian.sh
+++ linux-debian.sh
@@ -7,12 +7,12 @@
 ##################################################################################################
 
 cy='\033[96m' # Cyan Color
-rc='\033[0;31m' # Red Color
+rc='\033[1;31m' # Red Color
 c='\e[32m' # Green Color
 tc='\033[38;5;11m' # Yellow Color
 r='tput sgr0' #Reset Color
 
-echo -e "\n\n\n\n ${rc}
+echo -e "\n\n\n\n ${cy}
     _         _       ____       _
    / \  _   _| |_ ___/ ___|  ___| |_ _   _ _ __
   / _ \| | | | __/ _ \___ \ / _ \ __| | | | '_ |
@@ -23,11 +23,11 @@ echo -e "\n\n\n\n ${rc}
 "; $r
 sleep 1;
 
-echo -e "${cy}[INFO}${tc}Sebelum memulai menjalakan sistem auto konfigurasi ini pastikan anda berada dalam server linux";
+echo -e "${rc}[INFO] ${tc}Sebelum memulai menjalakan sistem auto konfigurasi ini pastikan anda berada dalam server linux";
 sleep 2;
-echo -e "${cy}[INFO}${tc}Script auto config ini hanya di khususkan untuk mengconfigurasi server linux based Debian";
+echo -e "${rc}[INFO] ${tc}Script auto config ini hanya di khususkan untuk mengconfigurasi server linux based Debian";
 sleep 2;
-echo -e "${cy}[INFO}${tc}Direkomendasi untuk menggunakan script ini hanya pengguna AWS";
+echo -e "${rc}[INFO] ${tc}Direkomendasi untuk menggunakan script ini hanya pengguna AWS";
 sleep 2;
 read -p "${tc}Apakah yakin menggunakan sistem auto konfigurasi? (y/n): " -r;
 if [[ $REPLY =~ ^[Yy]$ ]]; then
